887f0335080eee0373ef33340759dba2fcfa0ae4
Roberto <titorobe@yahoo.com>
2017-03-04 05:11:41 -0500

    03-04

--- app/view/auth/Login.js
+++ app/view/auth/Login.js
@@ -9,8 +9,8 @@ Ext.define('GirocheckMobile.view.auth.Login', {
         'Ext.field.Toggle',
         'Ext.data.identifier.Uuid'
     ],
-   
-    config: { 
+
+    config: {
         title: 'Login',
         cls: 'login',
         items: [
@@ -30,114 +30,119 @@ Ext.define('GirocheckMobile.view.auth.Login', {
                 }]
             },
             {
+                height: 50,
+                hidden: true,
+                html: 'Invalid Credentials',
+                style: { color: 'red', width: '100%', 'text-align': 'center' }
+            },
+            {
                 xtype: 'container',
                 items: [{
                     xtype: 'baseformpanel',
                     scrollable: null,
-                    items: [{
-                        xtype: 'fieldset',
-                        margin: '20 0 0 0',
-                        items: [{
-                            xtype: 'baseTextField',
-                            cls: 'loginfield',
-                            itemId: 'userName',
-                            name: 'user',
-                            title: 'Username', //i18n.login.usuario,
-                            placeHolder: 'Username'//i18n.login.enter_user
-                        }]
-                    }, {
+                    defaults: {
                         xtype: 'fieldset',
                         margin: '10 0 0 0',
-                        items: [{
-                            xtype: 'passwordField',
-                            labelWidth: 120,
-                            cls: 'loginfield',
-                            itemId: 'password',
-                            name: 'password',
-                            label: 'Password'// i18n.login.password
-                            //value: 'this is just for dev'
-                        }]
-                    }, {
-                        layout: {
-                            type: 'hbox',
-                            align:'center'
+                        defaults: {
+                            xtype: 'textfield',
+                            cls: 'loginfield'
+                        }
+                    },
+                    items: [
+                        {
+                            items: [{
+                                id: 'loginPin',
+                                name: 'pin',
+                                placeHolder: 'PIN'
+                            }]
                         },
-                        defaults:{
-                            margin: '10 0 0 0',
-                            style:{
-                                font:'normal 0.9em/1.23077em helvetica, arial, sans-serif'
-                            }
+                        {
+                            items: [{
+                                id: 'loginUser',
+                                name: 'user',
+                                placeHolder: 'Username'
+                            }]
+                        }, {
+                            items: [{
+                                inputType: 'password',
+                                id: 'loginPassword',
+                                name: 'password',
+                                placeHolder: 'Password'
+                            }]
+                        }, {
+                            xtype: 'panel',
+                            layout: {
+                                type: 'hbox',
+                                align: 'center'
+                            },
+                            defaults: {
+                                margin: '10 0 0 0',
+                                style: {
+                                    font: 'normal 0.9em/1.23077em helvetica, arial, sans-serif'
+                                }
+                            },
+                            items: [
+                                {
+                                    xtype: 'link',
+                                    url: 'http://google.com',
+                                    html: 'Forgot Password'
+                                },
+                                {
+                                    flex: 1
+                                }, {
+                                    xtype: 'togglefield',
+                                    itemId: 'rememberMe',
+                                    cls: 'rememberme',
+                                    labelWidth: 110,
+                                    label: 'Remember Me',// i18n.login.recordar,
+                                    value: 0,
+                                    listeners: {
+                                        change: 'onRememberMeChange'
+                                    }
+                                }
+                            ]
                         },
-                        items: [
-                             {
-                                xtype: 'link',
-                                url: 'http://google.com',
-                                html: 'Forgot Password' 
+
+                        {
+                            xtype: 'container',
+                            margin: '20 0 0 0',
+                            layout: {
+                                type: 'vbox',
+                                pack: 'center',
+                                align: 'stretch'
+                            },
+                            defaults: {
+                                margin: '0 0 5 0',
+                                iconAlign: 'center'
+                            },
+                            items: [{
+                                itemId: 'loginButton',
+                                xtype: 'basebutton',
+                                text: 'Login',
+                                iconCls: 'fa fa-sign-in',
+                                listeners: {
+                                    tap: 'doLogin'
+                                }
                             },
                             {
-                            flex:1
-                            },{
-                            xtype: 'togglefield',
-                            itemId: 'rememberMe',
-                            cls:'rememberme',
-                            labelWidth: 110, 
-                            label: 'Remember Me',// i18n.login.recordar,
-                            value: 0
-                        }
-                        ]
-                    },
-
-                    {
-                        xtype: 'container',
-                        margin: '20 0 0 0',
-                        layout: {
-                            type: 'vbox',
-                            pack: 'center',
-                            align: 'stretch'
-                        },
-                        defaults:{
-                            margin: '0 0 5 0',
-                            iconAlign: 'center'
-                        },
-                        items: [{
-                            itemId: 'loginButton',
-                            xtype: 'basebutton', 
-                            text: 'Login',
-                            iconCls: 'fa fa-sign-in', 
-                            listeners: {
-                                tap: 'doLogin'
+                                xtype: 'basebutton',
+                                cls: 'registerButton',
+                                text: 'Register',
+                                iconCls: 'pictos pictos-lightning',
+                                style: {
+                                    'background-color': '#000000'
+                                },
+                                listeners: {
+                                    tap: 'onRegister'
+                                }
                             }
-                        },
-                        { 
-                            xtype: 'basebutton',
-                            cls: 'registerButton',
-                            text: 'Register',
-                            iconCls: 'pictos pictos-lightning',
-                            style:{
-                                'background-color':'#000000'
-                            },
-                            listeners:{
-                                tap:'onRegister'
-                            }
+                            ]
                         }
-                        ]
-                    }
 
                     ]
-                } 
+                }
                 ]
             }
-        ], 
-        control: {
-            '#rememberMe': {
-                change: 'onRememberMeChange'
-            } 
-        }
-    }, 
-    onRememberMeChange: function (me, newValue, oldValue, eOpts) {
-        me.el.dom.getElementsByClassName('x-thumb-wrap-el')[0].style['background-color'] = me.getValue() ? '#148c7e' : '#C0C0C0';
-
-        //   Ext.getStore('localStore').updateData({'rememberme':(newValue === 1 ? true : false)});
-
+        ]
     }
 });
