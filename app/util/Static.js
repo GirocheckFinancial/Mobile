b3244ef8c6e2a6fac7fff5343b56aff998ba8989
Roberto <titorobe@yahoo.com>
2017-03-08 06:58:29 -0500

    Adding Static file

---
+++ app/util/Static.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+Ext.define('GirocheckMobile.utils.Static', {
+    alternateClassName: 'GirocheckMobile.Static',
+    singleton: true,
+    alias: 'widget.static',
+    config: {
+        mainMenu: null
+    },
+    constructor: function(config) {
+        this.initConfig(config);
+    },
+    createGetMainMenu:function(v){
+       if(this.getMainMenu() == null){ 
+          this.setMainMenu( Ext.create('GirocheckMobile.component.MainMenu') );
+       }
+       return this.getMainMenu();
+    },
+    updateMainManuName:function(name){
+      this.createGetMainMenu().down('#menuName').setText(name);
+    }
+});
+
